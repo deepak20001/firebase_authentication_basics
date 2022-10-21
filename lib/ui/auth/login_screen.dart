2f8632c561641237c8797fa996c839722a6764e0
Deepak <djamwal10@gmail.com>
2022-10-21 12:49:54 +0530

    adding texteditingcontrollers and formfield

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:authentication_firebase/widgets/round_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,14 +9,41 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login"),
       ),
-      body: Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: "Email",
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            RoundButton(
+              title: "Login",
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
