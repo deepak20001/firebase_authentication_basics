9eb1a3203fabd5b91a444c134b8ab26ea42b1876
Deepak <djamwal10@gmail.com>
2022-10-21 16:03:41 +0530

    add willPopScope widget as Scaffold parent when click to lowe android back button it will exit

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:authentication_firebase/ui/auth/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -24,93 +25,100 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Login"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
+    // add willPopScope widget when click to lowe android back button it will exit
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Login"),
+          automaticallyImplyLeading: false,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.email),
+                      ),
+
+                      // validating the field shouldn't stay empty
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter e-mail";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock_open),
+                      ),
+
+                      // validating the field shouldn't stay empty
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter password";
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // log("tapped");
+                  if (_formKey.currentState!.validate()) {}
+                },
+                child: Text("Login"),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(380, 50),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      prefixIcon: Icon(Icons.email),
-                    ),
-
-                    // validating the field shouldn't stay empty
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter e-mail";
-                      }
-                      return null;
+                  Text("Don't have an account ?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignupScreen(),
+                        ),
+                      );
                     },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      prefixIcon: Icon(Icons.lock_open),
-                    ),
-
-                    // validating the field shouldn't stay empty
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter password";
-                      }
-                      return null;
-                    },
+                    child: Text("Sign up"),
                   ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // log("tapped");
-                if (_formKey.currentState!.validate()) {}
-              },
-              child: Text("Login"),
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(380, 50),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account ?"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SignupScreen(),
-                      ),
-                    );
-                  },
-                  child: Text("Sign up"),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
