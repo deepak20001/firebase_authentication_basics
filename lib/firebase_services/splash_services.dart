64acbe06e62a2336ffb1b6dacc2f8ebcc993ba37
Deepak <djamwal10@gmail.com>
2022-10-21 11:25:51 +0530

    created periodic function of 3 sec

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,1 +1,12 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../ui/auth/login_screen.dart';
 
+class SplashServices {
+  void isLogin(BuildContext context) {
+    Timer.periodic(Duration(seconds: 3), (timer) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    });
+  }
+}
