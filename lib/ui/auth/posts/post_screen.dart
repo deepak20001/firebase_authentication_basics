44d0b6d231ccf9b9dfc72c3d83cc466b01bcf3b7
Deepak <djamwal10@gmail.com>
2022-10-22 13:56:57 +0530

    added logout functionality in post_screen.dart

--- lib/ui/auth/posts/post_screen.dart
+++ lib/ui/auth/posts/post_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../../../utils/utils.dart';
+import '../login_screen.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -8,11 +11,28 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("POST Screen"),
+        actions: [
+          IconButton(
+            // adding functionality in logout button for logging out
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              }).onError((error, stackTrace) {
+                Utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
+          SizedBox(width: 15),
+        ],
       ),
     );
   }
