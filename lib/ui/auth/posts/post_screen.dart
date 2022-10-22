89862f61a11c60cf5578e317d4119369360bdbfb
Deepak <djamwal10@gmail.com>
2022-10-23 02:43:12 +0530

    m1 to fetch data from firebase using FirebaseAnimatedList

--- lib/ui/auth/posts/post_screen.dart
+++ lib/ui/auth/posts/post_screen.dart
@@ -1,5 +1,7 @@
 import 'package:authentication_firebase/ui/auth/posts/add_posts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import '../../../utils/utils.dart';
 import '../login_screen.dart';
@@ -13,6 +15,7 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
+  final ref = FirebaseDatabase.instance.ref("Post");
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +36,24 @@ class _PostScreenState extends State<PostScreen> {
             icon: Icon(Icons.logout_outlined),
           ),
           SizedBox(width: 15),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            // automatically handles the animation stuff of firebase
+            child: FirebaseAnimatedList(
+              query: ref,
+              // when nothig will be there then this will be shown
+              defaultChild: Text("Loading"),
+              itemBuilder: (context, snapshot, animation, index) {
+                return ListTile(
+                  title: Text(snapshot.child("title").value.toString()),
+                  subtitle: Text(snapshot.child("id").value.toString()),
+                );
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
