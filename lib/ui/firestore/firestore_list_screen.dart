dd676b7fc8c507f03592b0ea782a3ceeeae854ff
Deepak <djamwal10@gmail.com>
2022-10-23 13:07:32 +0530

    created instance of firestore and stored data

--- lib/ui/firestore/firestore_list_screen.dart
+++ lib/ui/firestore/firestore_list_screen.dart
@@ -1,10 +1,137 @@
+import 'package:authentication_firebase/ui/auth/posts/add_posts.dart';
+import 'package:authentication_firebase/ui/firestore/add_firestore_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import '../../../utils/utils.dart';
+import '../auth/login_screen.dart';
 
-class FireStoreScreen extends StatelessWidget {
+class FireStoreScreen extends StatefulWidget {
   const FireStoreScreen({super.key});
 
   @override
+  State<FireStoreScreen> createState() => _FireStoreScreenState();
+}
+
+class _FireStoreScreenState extends State<FireStoreScreen> {
+  final auth = FirebaseAuth.instance;
+  final editController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Firestore Screen"),
+        actions: [
+          IconButton(
+            // adding functionality in logout button for logging out
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              }).onError((error, stackTrace) {
+                Utils().toastMessage(error.toString());
+              });
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
+          SizedBox(width: 15),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+
+/*******************************************************************************************************/
+          // Expanded(
+          //   child: StreamBuilder(
+          //     stream: ref.onValue,
+          //     builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+          //       // accessing the content of table using map
+          //       Map<dynamic, dynamic> map =
+          //           snapshot.data!.snapshot.value as dynamic;
+          //       List<dynamic> list = [];
+          //       list.clear();
+          //       list = map.values.toList();
+
+          //       if (!snapshot.hasData) {
+          //         return CircularProgressIndicator();
+          //       } else {
+          //         return ListView.builder(
+          //           itemCount: snapshot.data!.snapshot.children.length,
+          //           itemBuilder: (context, index) {
+          //             return ListTile(
+          //               title: Text(list[index]["title"]),
+          //             );
+          //           },
+          //         );
+          //       }
+          //     },
+          //   ),
+          // ),
+
+/*******************************************************************************************************/
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text("aatsfd"),
+                  );
+                }),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddFirestoreDataScreen(),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+/*****************************************************************************************/
+  Future<void> showMyDialog(String title, String id) async {
+    // already showing the past text on dialog box to update
+    editController.text = title;
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Update"),
+          content: Container(
+            child: TextField(
+              controller: editController,
+              decoration: InputDecoration(hintText: "Edit here"),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Update"),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
