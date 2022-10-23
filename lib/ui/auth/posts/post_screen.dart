10edc2613fa838f6eddf6a6a56f1eeb77f0362c1
Deepak <djamwal10@gmail.com>
2022-10-23 10:25:14 +0530

    added search functionality to filter the needed info

--- lib/ui/auth/posts/post_screen.dart
+++ lib/ui/auth/posts/post_screen.dart
@@ -16,11 +16,13 @@ class PostScreen extends StatefulWidget {
 class _PostScreenState extends State<PostScreen> {
   final auth = FirebaseAuth.instance;
   final ref = FirebaseDatabase.instance.ref("Post");
+  final searchFilter = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("POST Screen"),
         actions: [
           IconButton(
@@ -40,33 +42,50 @@ class _PostScreenState extends State<PostScreen> {
       ),
       body: Column(
         children: [
-/*******************************************************************************************************/
-          Expanded(
-            child: StreamBuilder(
-              stream: ref.onValue,
-              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-                // accessing the content of table using map
-                Map<dynamic, dynamic> map =
-                    snapshot.data!.snapshot.value as dynamic;
-                List<dynamic> list = [];
-                list.clear();
-                list = map.values.toList();
-
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.snapshot.children.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(list[index]["title"]),
-                      );
-                    },
-                  );
-                }
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextFormField(
+              controller: searchFilter,
+              decoration: InputDecoration(
+                hintText: "Search",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (String value) {
+                setState(() {});
               },
             ),
           ),
+/*******************************************************************************************************/
+          // Expanded(
+          //   child: StreamBuilder(
+          //     stream: ref.onValue,
+          //     builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+          //       // accessing the content of table using map
+          //       Map<dynamic, dynamic> map =
+          //           snapshot.data!.snapshot.value as dynamic;
+          //       List<dynamic> list = [];
+          //       list.clear();
+          //       list = map.values.toList();
+
+          //       if (!snapshot.hasData) {
+          //         return CircularProgressIndicator();
+          //       } else {
+          //         return ListView.builder(
+          //           itemCount: snapshot.data!.snapshot.children.length,
+          //           itemBuilder: (context, index) {
+          //             return ListTile(
+          //               title: Text(list[index]["title"]),
+          //             );
+          //           },
+          //         );
+          //       }
+          //     },
+          //   ),
+          // ),
+
 /*******************************************************************************************************/
           Expanded(
             // automatically handles the animation stuff of firebase
@@ -75,10 +94,23 @@ class _PostScreenState extends State<PostScreen> {
               // when nothig will be there then this will be shown
               defaultChild: Text("Loading"),
               itemBuilder: (context, snapshot, animation, index) {
-                return ListTile(
-                  title: Text(snapshot.child("title").value.toString()),
-                  subtitle: Text(snapshot.child("id").value.toString()),
-                );
+                final title = snapshot.child("title").value.toString();
+
+                if (searchFilter.text.isEmpty) {
+                  return ListTile(
+                    title: Text(snapshot.child("title").value.toString()),
+                    subtitle: Text(snapshot.child("id").value.toString()),
+                  );
+                } else if (title
+                    .toLowerCase()
+                    .contains(searchFilter.text.toLowerCase())) {
+                  return ListTile(
+                    title: Text(snapshot.child("title").value.toString()),
+                    subtitle: Text(snapshot.child("id").value.toString()),
+                  );
+                } else {
+                  return Container();
+                }
               },
             ),
           ),
